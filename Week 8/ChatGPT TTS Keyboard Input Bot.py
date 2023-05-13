0c79f64ed706b16e1cd36b6d23e775063de52fa3
Muchiri <79175191+kr-muchiri@users.noreply.github.com>
2023-05-13 00:14:33 -0400

    ChatGPT enabled bot- keyboard input

---
+++ Week 8/ChatGPT TTS Keyboard Input Bot.py
@@ -0,0 +1,58 @@
+import openai
+import os
+import pygame
+from google.cloud import texttospeech
+
+# Initialize OpenAI API
+openai.api_key = "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"
+
+# Function to convert text to speech using Google Text-to-Speech API
+def text_to_speech(text):
+    client = texttospeech.TextToSpeechClient()
+    input_text = texttospeech.SynthesisInput(text=text)
+    voice = texttospeech.VoiceSelectionParams(language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE)
+    audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.LINEAR16)
+
+    response = client.synthesize_speech(input=input_text, voice=voice, audio_config=audio_config)
+
+    with open("output.wav", "wb") as out:
+        out.write(response.audio_content)
+
+    return "output.wav"
+
+
+# Function to get response from OpenAI's GPT-3 API
+def get_chatgpt_response(input_text):
+    chatgpt_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=input_text,
+        max_tokens=1000,
+        n=10,
+        stop=None,
+        temperature=0.7,
+    )
+
+    return chatgpt_response.choices[0].text.strip()
+
+
+# Main function to get user input, generate responses using ChatGPT API, and play text-to-speech audio
+def main():
+    user_input = input("Ask me a question: ")
+    print("Getting chatbot response...")
+
+    chatgpt_response = get_chatgpt_response(user_input)
+    print(f"ChatGPT: {chatgpt_response}")
+
+    print("Converting text to speech...")
+    chatgpt_audio_file = text_to_speech(chatgpt_response)
+
+    pygame.mixer.init()
+
+    print("Playing ChatGPT response...")
+    pygame.mixer.music.load(chatgpt_audio_file)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+if __name__ == "__main__":
+    main()
