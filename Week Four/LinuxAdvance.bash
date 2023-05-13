c345c417973dfba3990ddf4a1232117048817916
Muchiri <79175191+kr-muchiri@users.noreply.github.com>
2023-05-12 21:10:03 -0400

    This script shows advanced understanding of Linux

---
+++ Week Four/LinuxAdvance.bash
@@ -0,0 +1,103 @@
+# Find files containing a specific pattern
+grep -r "pattern" /path/to/directory
+
+# Count the number of lines in a file
+wc -l file.txt
+
+# Sort lines in a file
+sort file.txt
+
+# Filter and transform text with AWK
+awk '{print $1}' file.txt
+
+# Archive files using tar with compression
+tar -czvf archive.tar.gz file1.txt file2.txt
+
+# Search for a package in the package manager
+apt search package-name
+
+# Monitor network traffic with tcpdump
+tcpdump -i eth0 -n
+
+# Monitor disk I/O usage with iostat
+iostat -d 1
+
+# Monitor system resource usage with sar
+sar -u 1
+
+# Generate a random password
+openssl rand -base64 12
+# Find files containing a specific pattern
+grep -r "pattern" /path/to/directory
+
+# Count the number of lines in a file
+wc -l file.txt
+
+# Sort lines in a file
+sort file.txt
+
+# Filter and transform text with AWK
+awk '{print $1}' file.txt
+
+# Archive files using tar with compression
+tar -czvf archive.tar.gz file1.txt file2.txt
+
+# Search for a package in the package manager
+apt search package-name
+
+# Monitor network traffic with tcpdump
+tcpdump -i eth0 -n
+
+# Monitor disk I/O usage with iostat
+iostat -d 1
+
+# Monitor system resource usage with sar
+sar -u 1
+
+# Generate a random password
+openssl rand -base64 12
+
+# Check disk usage of directories
+du -sh /path/to/directory
+
+# View active network connections
+netstat -tuln
+
+# Monitor CPU usage with top
+top
+
+# Display process tree hierarchy
+pstree
+
+# Monitor memory usage with free
+free -h
+
+# Monitor disk activity with iotop
+iotop
+
+# Check system uptime
+uptime
+
+# Check the listening ports
+ss -tunlp
+
+# Monitor system temperature
+sensors
+
+# List logged-in users
+who
+
+# Monitor file system changes
+inotifywait -r -m /path/to/directory
+
+# Generate a random password with pwgen
+pwgen 12 1
+
+# Check system load averages
+w
+
+# Check system kernel version
+uname -r
+
+# Check network interface configuration
+ip addr show
